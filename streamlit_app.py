8969a3642cb735de077e8d674bce533360550ccd
liuh91708-design <liuh91708@gmail.com>
2026-04-26 00:11:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,8 +183,7 @@ else:
     display_df = df
 
 st.dataframe(display_df.sort_values("Score", ascending=False), use_container_width=True),
-use_container_width=True
-)
+use_container_width=True)
 
 # 市场结构图
 st.subheader("🧭 市场结构图")
